e5ae3567b6c895b915efc65d582e79875190a327
David Morris <nondebtdasthisdyou@gmx.net>
2023-07-22 10:00:00 -0400

    Implement VGG-19 based neural style transfer

--- style_transfer.py
+++ style_transfer.py
@@ -1,92 +1,136 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+#!/usr/bin/env python3
 """
-This script is a placeholder for a neural style transfer implementation.
-A real implementation would use a pre-trained convolutional neural network (like VGG)
-to extract content and style features from images and then optimize a new image
-to match the content of one and the style of another.
-
-This is a computationally intensive process and is simplified here for demonstration.
+This script implements neural style transfer using a pre-trained VGG-19 network.
+It applies the style of one image to the content of another.
 """
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from PIL import Image
+import torchvision.transforms as transforms
+import torchvision.models as models
 import argparse
 import os
 
-class StyleTransferModel:
-    """A mock model for demonstrating neural style transfer concepts."""
+# --- Configuration ---
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+IMAGE_SIZE = 512 if torch.cuda.is_available() else 128  # Use smaller size on CPU
 
-    def __init__(self, vgg_model_path="./vgg19.pth"):
-        """Initializes the model, pretending to load a pre-trained VGG network."""
-        self.vgg_path = vgg_model_path
-        if not os.path.exists(self.vgg_path):
-            print(f"VGG model not found at {self.vgg_path}. Creating a mock file.")
-            self._create_mock_model_file()
+# --- Image Loading and Preprocessing ---
+loader = transforms.Compose([
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+    transforms.ToTensor(),
+])
 
-    def _create_mock_model_file(self):
-        """Creates a dummy model file."""
-        with open(self.vgg_path, "w") as f:
-            f.write("mock vgg19 model data")
+def load_image(image_path):
+    if not os.path.exists(image_path):
+        print(f"Warning: Creating dummy image at {image_path}")
+        Image.new("RGB", (100, 100), color="blue").save(image_path)
+    image = Image.open(image_path).convert("RGB")
+    image = loader(image).unsqueeze(0)
+    return image.to(DEVICE, torch.float)
 
-    def _load_and_preprocess_image(self, image_path):
-        """Placeholder for loading and preprocessing an image."""
-        if not os.path.exists(image_path):
-            print(f"Warning: Image file not found at {image_path}. Creating a dummy file.")
-            with open(image_path, "w") as f:
-                f.write("dummy image data")
-        # In a real implementation, this would use Pillow to open the image,
-        # resize it, and convert it to a tensor.
-        print(f"Loaded and preprocessed image: {os.path.basename(image_path)}")
-        return f"tensor_of_{os.path.basename(image_path)}"
+# --- VGG Model and Feature Extraction ---
+class VGG(nn.Module):
+    def __init__(self):
+        super(VGG, self).__init__()
+        # Import VGG19 features, only the conv layers
+        self.chosen_features = ["0", "5", "10", "19", "28"]
+        self.model = models.vgg19(pretrained=True).features[:29]
 
-    def transfer_style(self, content_path, style_path, output_path, iterations=100):
-        """Simulates the style transfer process."""
-        print("\n--- Starting Neural Style Transfer ---")
-        print(f"Content Image: {content_path}")
-        print(f"Style Image: {style_path}")
-        print(f"Output Image: {output_path}")
-        print(f"Iterations: {iterations}")
+    def forward(self, x):
+        features = []
+        for layer_num, layer in enumerate(self.model):
+            x = layer(x)
+            if str(layer_num) in self.chosen_features:
+                features.append(x)
+        return features
 
-        # 1. Load images
-        content_img_tensor = self._load_and_preprocess_image(content_path)
-        style_img_tensor = self._load_and_preprocess_image(style_path)
+# --- Loss Functions ---
+class ContentLoss(nn.Module):
+    def __init__(self, target):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
 
-        # 2. Initialize a target image (e.g., from content image or noise)
-        target_img_tensor = content_img_tensor
+    def forward(self, input):
+        self.loss = nn.functional.mse_loss(input, self.target)
+        return input
 
-        # 3. Optimization loop (simulated)
-        print("\nSimulating optimization process...")
-        for i in range(iterations):
-            # In a real implementation, this loop would:
-            # - Pass images through the VGG network to get feature maps.
-            # - Calculate content loss between target and content feature maps.
-            # - Calculate style loss between target and style feature maps.
-            # - Compute total loss and update the target image.
-            if (i + 1) % 20 == 0:
-                print(f"  Iteration {i + 1}/{iterations}... Loss: {1.0 / (i + 1):.4f}")
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = self.gram_matrix(target_feature).detach()
 
-        # 4. Post-process and save the target image
-        print("\nOptimization finished.")
-        self._save_image(output_path)
+    def forward(self, input):
+        G = self.gram_matrix(input)
+        self.loss = nn.functional.mse_loss(G, self.target)
+        return input
 
-    def _save_image(self, output_path):
-        """Saves the generated image."""
-        # This would convert the tensor back to an image and save it.
-        with open(output_path, "w") as f:
-            f.write("stylized image data")
-        print(f"Stylized image saved to: {output_path}")
+    @staticmethod
+    def gram_matrix(input):
+        batch_size, f_map_num, h, w = input.size()
+        features = input.view(batch_size * f_map_num, h * w)
+        G = torch.mm(features, features.t())
+        return G.div(batch_size * f_map_num * h * w)
 
-def main():
-    """Parses arguments and runs the style transfer."""
-    parser = argparse.ArgumentParser(description="Apply the style of one image to another.")
-    parser.add_argument("--content_image", type=str, required=True, help="Path to the content image.")
-    parser.add_argument("--style_image", type=str, required=True, help="Path to the style image.")
-    parser.add_argument("--output_image", type=str, default="stylized_image.jpg", help="Path to save the output image.")
-    parser.add_argument("--iterations", type=int, default=100, help="Number of optimization iterations.")
+# --- Main Transfer Function ---
+def run_style_transfer(content_img, style_img, num_steps=300, style_weight=1000000, content_weight=1):
+    print(f"Running on device: {DEVICE}")
+    model = VGG().to(DEVICE).eval()
+    optimizer = optim.LBFGS([content_img.requires_grad_()])
+
+    print("Building the style transfer model...")
+    run = [0]
+    while run[0] <= num_steps:
+        def closure():
+            content_img.data.clamp_(0, 1)
+            optimizer.zero_grad()
+            
+            content_features = model(content_img)
+            style_features = model(style_img)
+
+            style_score = 0
+            content_score = 0
+
+            for cf, sf in zip(content_features, style_features):
+                content_score += nn.functional.mse_loss(cf, sf)
+                style_score += StyleLoss.gram_matrix(cf).mean()
+
+            style_score *= style_weight
+            content_score *= content_weight
+            
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print(f"  Run {run[0]}: Style Loss: {style_score.item():4f} Content Loss: {content_score.item():4f}")
+            
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    content_img.data.clamp_(0, 1)
+    return content_img
+
+# --- Main Execution ---
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Neural Style Transfer")
+    parser.add_argument("--content_image", required=True, help="Path to content image")
+    parser.add_argument("--style_image", required=True, help="Path to style image")
+    parser.add_argument("--output_image", default="stylized_image.png", help="Path to save the output image")
+    parser.add_argument("--steps", type=int, default=200, help="Number of optimization steps")
     args = parser.parse_args()
 
-    model = StyleTransferModel()
-    model.transfer_style(args.content_image, args.style_image, args.output_image, args.iterations)
+    content_image = load_image(args.content_image)
+    style_image = load_image(args.style_image)
 
-if __name__ == "__main__":
-    main()
+    output = run_style_transfer(content_image, style_image, num_steps=args.steps)
+
+    # Save the result
+    unloader = transforms.ToPILImage()
+    image = output.cpu().clone().squeeze(0)
+    image = unloader(image)
+    image.save(args.output_image)
+    print(f"\nStyle transfer complete. Image saved to {args.output_image}")
